55827f0d626d70903b2cb1fc5c76614091ab2696
Neonyao <114361176+Neonyao@users.noreply.github.com>
2022-10-03 19:29:46 -0400

    Update Polynomial.java

--- Polynomial.java
+++ Polynomial.java
@@ -1,50 +1,98 @@
+import java.io.File;
+import java.util.Arrays;
+import java.lang.Math;
+import java.util.Scanner;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+
+
 public class Polynomial {
 	
 	double[] coef;
+	int[] exp;
 
 	public Polynomial() {
-        coef = new double[] {0};
-    }
+		this.coef = new double[1];
+		this.coef[0] = 0;
+		this.exp = new int[1];
+		this.exp[0] = 0;
+	}
     
 
-	public Polynomial(double arg[]) {
-		coef = new double[arg.length];
-		for(int i = 0; i < arg.length; i++) {
-			coef[i] = arg[i];
+    	public Polynomial(double[] coef, int[] exp) {
+		this.coef = coef;
+		this.exp = exp;
+   	}
+	
+	
+    	public Polynomial(File file) throws IOException {
+		
+		FileReader readed = new FileReader(file);
+		BufferedReader textReader = new BufferedReader(readed);
+		
+		String text = textReader.readLine();
+		String[] str = text.split("\\+|-");
+
+		double returnCoef[] = new double[str.length];
+		int returnExp[] = new int[str.length];
+		int i = 0;
+		
+		for (String str) {
+		    if (str.contains("x")) {
+			String[] tempSplit = str.split("x");
+			returnCoef[i] = Double.parseDouble(tempSplit[0]);
+			returnExp[i] = Integer.parseInt(tempSplit[1]);
+		    }
+		    else {
+			returnCoef[i] = Double.parseDouble(str);
+			returnExp[i] = 0;
+		    }
+		    i++;
 		}
-	}
+		this.coeff = returnCoef;
+		this.exp = returnExp;
+    }		
+	
+	
 	
 
     public Polynomial add(Polynomial p2) {
         
-	  int len1 = this.coef.length;
+	int len1 = this.coef.length;
 
         int len2 = p2.coef.length;
 
         double[] sum = new double[Math.max(len1, len2)];
+	int[] sexp = new int[Math.max(len1, len2)];
 
         for (int i = 0; i < len1; i++)
         {
             sum[i] += this.coef[i];
+	    sexp[i] += this.exp[i];
         }
 	
-	if (len1 < len2){
+	
         for (int i = 0; i < len2; i++)
         {
             sum[i] += p2.coef[i];
+	    sexp[i] += p2.exp[i];
         }
-	}
+	
 
-        return new Polynomial(sum);
+        return new Polynomial(new Polynomial(sum,sexp));
     }
+	
+	
 
 
     public double evaluate(double x)
     {
         double sum = 0;
-        for(int i = 0; i<coef.length; i++)
+        for(int i = 0; i<this.coef.length; i++)
         {
-            sum+=(coef[i]*Math.pow(x,i));
+            sum+=(this.coef[i]*Math.pow(x, this.exp[i]));
         }
         return sum;
     }
@@ -53,4 +101,45 @@ public class Polynomial {
 		if (evaluate(n) == 0) return true;
 		return false;
 	}
+	
+	
+       public Polynomial multiply(Polynomial p) {
+	       
+	       
+	       
+	       
+	       
+	       
+       }
+	
+	
+	
+
+	public void saveToFile(File f) throws Exception{
+		String out = "";
+		for (int i = 0; i < coef.length; i++) {
+		   if (this.exp[i] == 0) {
+			if (this.coef[i] >= 0) {
+			    out += "+" + Double.toString(this.coef[i]);
+			}
+			else {
+			    out += Double.toString(this.coef[i]);
+			}
+		    }
+		    else {
+			if (this.coef[i] >= 0) {
+			    out += "+" + Double.toString(this.coef[i]) + "x" + Integer.toString(this.exp[i]);
+			}
+			else {
+			    out += Double.toString(this.coef[i]) + "x" + Integer.toString(this.exp[i]);
+			}
+            }
+		}
+	    FileWriter newfile; 
+	    newfile = new FileWriter(f);
+	    newfile.write(out);
+	    newfile.close();
+	}
+	
+    
 }
